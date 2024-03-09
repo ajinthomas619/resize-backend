1b117043e073dba18da62fc90afe4adc2d67e7f9
ajin thomas <ajinthomas619@gmail.com>
2024-03-09 19:47:16 +0530

    hao commit

--- index.js
+++ index.js
@@ -117,3 +117,5 @@ app.use(express.static(path.join(__dirname, 'client/dist')));
 
   const PORT = 8080
   app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+
+  module.exports = app;
