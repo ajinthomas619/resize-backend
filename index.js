6aa8f6420bc85043bf4e99da83968c391fcb5e35
ajin thomas <ajinthomas619@gmail.com>
2024-03-09 18:18:50 +0530

    f commit

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ const corsOption = {
 }
 app.use(cors(corsOption))
 app.use(express.json())
-mongoose.connect('mongodb://127.0.0.1:27017/apidemo', {
-useNewUrlParser: true, useUnifiedTopology: true 
+mongoose.connect("mongodb+srv://ajinthomas619:Motog31@cluster0.u9qv5iq.mongodb.net/apidemo",{
+useNewUrlParser: true,
+useUnifiedTopology:true
+}).then(() => {
+  console.log("Connected to MongoDB Atlas");
+}).catch((error) => {
+  console.error("Error connecting to MongoDB Atlas: ", error);
 });
 const db = mongoose.connection
 db.on('error',console.error.bind(console,'Mongodb connect error'))
